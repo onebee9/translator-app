737d55c3e95533086f7b42f6f7206c3aacc43a85
onebee9 <oduibifuro@gmail.com>
2020-06-15 15:22:47 -0400

    created the UI

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 //all dependencies should be at the top.
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const express = require("express");
 const got = require("got");
 const uuidv4 = require("uuid/v4");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
-
 const app = express();
-
-app.use(bodyParser.json()); //app.use is for middlewares, bodyparser.json converts the bosy of an incoming post body to Json
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname + "/FE/")));
 
 app.post("/api/translate", async (request, response) => {
-  const { to: language } = request.query; // this is a destructured version of const language = request.query.to, format is const{key}=request.query
+  const language = request.body.language;
+  console.log(language);
+  const textToConvert = request.body.text;
+  console.log(textToConvert)
 
-  const { string: textToConvert } = request.body;
-  //const languageCode = request.body.language;
-  if (!language) { //catches all falsy values
-   
+  //catch all falsy values
+  if (!language) {
     const responseObj = {
       status: "failed",
       message: "Language code cannot be empty",
     };
     response.status(400);
     response.json(responseObj);
-  } else if (!textToConvert) {//catches all falsy values
-    
+  } else if (!textToConvert) {
+    //catches all falsy values
     let responseObj = {
       status: "failed",
       message: "Please enter a string to convert",
@@ -43,35 +46,66 @@ app.post("/api/translate", async (request, response) => {
     let options = {
       headers: {
         "Ocp-Apim-Subscription-Key": process.env.KEY,
+        "Ocp-Apim-Subscription-Region":'canadacentral',
         "Content-type": "application/json",
-        "X-ClientTraceId": uuidv4().toString(),
+        "X-ClientTraceId": uuidv4().toString()
       },
       body: JSON.stringify(sendToApi),
     };
 
     try {
-      //console.log('alibaba');
+      console.log("alibaba");
       const result = await got.post(
-        `${process.env.ENDPOINT}/translate?api-version=3.0&to=${language}`,
-        options
-      );
-      console.log(result.body);
+        `${process.env.ENDPOINT}translate?api-version=3.0&to=${language}`,
+        options);
+
+
       let results = JSON.parse(result.body);
-      //let g= results[0].translations[0].text;
       let responseObj = {
         translatedText: results[0].translations[0].text,
-        language, // because both the key and the value have the same name it can be represented by just one "key", it represents language:language.
+        language, // language:language
         time: new Date(),
       };
+      console.log(responseObj);
+
       response.status(200);
-      response.json(responseObj); // you cant sne d javascript object, it has to be a json string
+      response.json(responseObj);
+
     } catch (error) {
       console.log(error);
       response.status(400);
       response.send(error);
-      //=> 'Internal server error ...'
     }
   }
 });
 
-app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
+app.get("/api/languages", async (request, response) => {
+  try {
+    const Languages = await got(
+      `${process.env.ENDPOINT}/languages?api-version=3.0&scope=translation`
+    );
+
+    const language_data = JSON.parse(Languages.body);
+    const language_array = Object.entries(language_data.translation);
+
+    const mapper = (item) => {
+      return {
+        key: item[0],
+        name: item[1].name,
+      };
+    };
+
+    const processed_languages = language_array.map(mapper); 
+    response.status(200);
+    response.json(processed_languages);
+
+  } catch (error) {
+    console.log(error);
+    response.status(400);
+    response.send(error);
+  }
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
